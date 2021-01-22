7cf6aa26b7344cfab429b5427ac27fc99fcf4d24
E-Martinez77 <emartizzle2185@me.com>
2021-01-22 16:20:33 -0700

    click function added

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+var searchButton = document.getElementById("searchButton");
+var searchBox = document.getElementById("searchBox");
+
+var previousCities = document.getElementById("previousCities");
+
+var cityArray = [];
+
+$("#searchButton").on("click", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  var city = $("#searchBox").val().trim();
+
+  cityArray.push(city);
+
+  //create a new element when there's a click.
+
+  // $("#random-number").prepend("<br><hr>" + lottoNumber);
+
+  // $("#previousCities").append("<li>") + city;
+  $("#previousCities").append("<li>" + city + "</li>");
+
+  //once I have that element I want to add in city
+  // $("ol").append("<li>Appended item</li>");
+
+  console.log(city);
+  console.log(cityArray);
+});
+
+// $("p").on("click", function(){
+//   alert("The paragraph was clicked.");
+// });
+
 var APIKey = "166a433c57516f5b9b96241930f99095ee709c216db0ae2";
 
 var queryURL =
