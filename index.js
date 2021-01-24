fc5525da6690697103faa029d5120a38a47452ea
E-Martinez77 <emartizzle2185@me.com>
2021-01-24 09:57:26 -0700

    Updated formula

--- index.js
+++ index.js
@@ -13,40 +13,44 @@ $("#searchButton").on("click", function (event) {
 
   cityArray.push(city);
 
-  //create a new element when there's a click.
-
-  // $("#random-number").prepend("<br><hr>" + lottoNumber);
-
-  // $("#previousCities").append("<li>") + city;
-  $("#previousCities").append("<li>" + city + "</li>");
-
-  //once I have that element I want to add in city
-  // $("ol").append("<li>Appended item</li>");
-
   console.log(city);
   console.log(cityArray);
+
+  citySearch();
+  cityRender();
 });
 
-// $("p").on("click", function(){
-//   alert("The paragraph was clicked.");
-// });
+function cityRender() {
+  //set search to be empty
+  $("#previousCities").empty();
+  $("#searchBox").val("");
+  for (let i = 0; i < cityArray.length; i++) {
+    var li = $("<li>");
+    li.attr("data-city", cityArray[i]);
+    li.text(cityArray[i]);
+    $("#previousCities").append(li);
+  }
+}
 
-var APIKey = "166a433c57516f5b9b96241930f99095ee709c216db0ae2";
+function citySearch() {
+  var city = $("#searchBox").val().trim();
+  var APIKey = "b9b96241930f99095ee709c216db0ae2";
 
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Bujumbura,Burundi&appid=" +
-  APIKey;
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}) // We store all of the retrieved data inside of an object called "response"
-  .then(function (response) {
-    // Log the queryURL
-    console.log(queryURL);
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }) // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+      // Log the queryURL
 
-    // Log the resulting object
-    console.log(response);
-  });
+      // Log the resulting object
+      console.log(response);
+    });
+}
